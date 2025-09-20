0baf3df5132a2d7fb90d0a1335b93fcbe27800d0
unknown <you@example.com>
2025-09-20 10:29:32 +0300

    Fix database configuration for Railway deployment

--- models/index.js
+++ models/index.js
@@ -6,14 +6,14 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config.js');
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.database.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.database.use_env_variable], config.database);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, config.database);
 }
 
 fs
